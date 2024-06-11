cf2bee8a24c0ff5da452f987367275caedb32f58
Bharat Yadav <96131138+bharatydv@users.noreply.github.com>
2024-06-11 11:58:07 +0530

    Delete template directory

--- template/groups/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from groups.models import Group, GroupMember
-
-# Register your models here.
-class GroupMemberInline(admin.TabularInline):
-    model = GroupMember
-
-admin.site.register(Group)
